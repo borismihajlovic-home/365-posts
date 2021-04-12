b87680c4e3b667f928c2c46e71e12a39247df8d3
boris_mihajlovic <mihajlovicboris@gmail.com>
2021-04-12 22:16:05 +0200

    small refactor

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,53 @@
 <template>
-  <router-view/>
+	<router-view/>
 </template>
 
+<script>
+export default {
+	methods: {
+		fetchPassResponse(response){
+			if(response.ok){
+				return response.json();
+			}
+		},
+		getPosts(){
+			fetch('https://jsonplaceholder.typicode.com/posts/')
+			.then(this.fetchPassResponse)
+			.then(responseData => {
+				responseData.forEach(item => {
+					this.getUser(item.userId).then(data=>{
+						item.userData=data;
+						this.$store.commit('addPostToArray', item);
+					});	
+				});
+				this.isLoading=false;
+			}).catch(er => {
+				console.log(er);
+			});
+		},
+		getUser(userId){
+			return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+			.then(this.fetchPassResponse)
+			.then(responseData => {
+				this.$store.commit('addAuthorToArray', responseData);
+				return responseData;
+			}).catch(er => {
+				console.log(er);
+			});
+		}
+	},
+	created(){
+		this.getPosts();
+	}
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #000;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #000;
 }
 </style>
--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -14,40 +14,6 @@ import Post from '../components/Post'
 export default {
 	name: 'Posts',
 	components: { Header, Post },
-	methods: {
-		fetchPassResponse(response){
-			if(response.ok){
-				return response.json();
-			}
-		},
-		getPosts(){
-			fetch('https://jsonplaceholder.typicode.com/posts/')
-			.then(this.fetchPassResponse)
-			.then(responseData => {
-				responseData.forEach(item => {
-					this.getUser(item.userId).then(data=>{
-						item.userData=data;
-						this.$store.commit('addPostToArray', item);
-					});	
-				});
-				this.isLoading=false;
-			}).catch(er => {
-				console.log(er);
-			});
-		},
-		getUser(userId){
-			return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-			.then(this.fetchPassResponse)
-			.then(responseData => {
-				this.$store.commit('addAuthorToArray', responseData);
-				return responseData;
-			}).catch(er => {
-				console.log(er);
-			});
-		}
-	},
-	created(){
-		this.getPosts();
-	}
+	
 }
 </script>
