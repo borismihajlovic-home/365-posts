ae9663921ce14e4f86fbe41a9ae5784391b02663
boris_mihajlovic <mihajlovicboris@gmail.com>
2021-04-14 22:29:29 +0200

    mock comments

--- src/views/SinglePost.vue
+++ src/views/SinglePost.vue
@@ -5,14 +5,6 @@
 		<div class="max-w-750px bg-transparent mx-auto border-black border-l border-r">	
 			<Header></Header>
 			<PostUserHolder :isPostList="false" :post="post"></PostUserHolder>
-			
-			<!-- post user holder 
-				<section class="flex justify-end px-3 items-center h-20">
-					<div class="flex cursor-pointer items-center hover:underline" @click="$store.dispatch('openAuthorProfile', {postId:usersPost.id, authorId: usersPost.userId})">
-						<span class="mr-3 font-black">{{ usersPost.userData.username }}</span> 
-						<img class="rounded-full h-10" src="https://via.placeholder.com/150" alt="userImg" />
-					</div>
-				</section> -->
 
 			<!-- post section  -->
 			<section class="text-left p-5 border-black border-t">
@@ -24,14 +16,31 @@
 			<section  class="bg-yellow-200 text-left border-b border-black">
 				<div class="px-7 py-5 font-black text-blue-400 text-3xl border-t border-gray-400">Comments</div>
 				
-				<!-- comment -->
-				<!-- <div v-for="comment in usersPost.comments" :key="comment.id" class="flex justify-between p-5 pb-10 border-t border-gray-400">
+				<!-- comment odkomentarisati kada se "dovuku" komentari -->
+				<div v-for="comment in comments" :key="comment.id" class="flex justify-between p-5 pb-10 border-t border-gray-400">
+					<div class="image w-1/12"><img class="rounded-full h-8 w-8" src="https://via.placeholder.com/150" alt="userImg" /></div>
+					<div class="px-2">
+						<div class="text-blue-300 text-xs mb-2 font-black">{{ comment.commentUser }}</div>
+						<div>{{ comment.body }}</div>
+					</div>
+				</div>
+				
+				<!-- Dummy comments -->
+				<!-- <div class="flex justify-between p-5 pb-10 border-t border-gray-400">
+					<div class="image w-1/6"><img class="rounded-full h-8 w-8" src="https://via.placeholder.com/150" alt="userImg" /></div>
+					<div class="px-2">
+						<div class="text-blue-300 text-xs mb-2 font-black">Bret</div>
+						<div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, atque ab velit, hic dolore reprehenderit cumque corporis voluptate maiores repudiandae optio eligendi iste aspernatur! Eveniet modi obcaecati magnam itaque aut.</div>
+					</div>
+				</div>
+				<div class="flex justify-between p-5 pb-10 border-t border-gray-400">
 					<div class="image w-1/6"><img class="rounded-full h-8 w-8" src="https://via.placeholder.com/150" alt="userImg" /></div>
 					<div class="px-2">
 						<div class="text-blue-300 text-xs mb-2 font-black">Bret</div>
 						<div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, atque ab velit, hic dolore reprehenderit cumque corporis voluptate maiores repudiandae optio eligendi iste aspernatur! Eveniet modi obcaecati magnam itaque aut.</div>
 					</div>
 				</div> -->
+				<!-- END Dummy comments -->
 				
 			</section>
 		</div>
@@ -48,17 +57,28 @@ export default {
 	components: { Header, Profile, PostUserHolder },
 	data(){
 		return{
-			post:{}
+			post:{},
+			comments: []
 		}
 	},
 	methods:{
 		setPost(postsArray){
 			const clickedPost = postsArray.filter(item => item.id == this.$route.params.postId && item.userId == this.$route.params.userId);
 			this.post = clickedPost[0];
+		},
+		getComments(){
+			// TODO: Check the data that is returned on https://jsonplaceholder.typicode.com/posts/1/comments and implement comments
+			this.comments = [
+				{commentUser: 'Boeis', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, atque ab velit, hic dolore reprehenderit cumque corporis'},
+				{commentUser: 'Boeis', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, atque ab velit, hic dolore reprehenderit cumque corporis'},
+				{commentUser: 'Boeis', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, atque ab velit, hic dolore reprehenderit cumque corporis'},
+				{commentUser: 'Boeis', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, atque ab velit, hic dolore reprehenderit cumque corporis'}
+			]
 		}
 	},
 	created(){
 		this.setPost(this.$store.getters.getAllPosts);
+		this.getComments();
 	}
 }
 </script>
